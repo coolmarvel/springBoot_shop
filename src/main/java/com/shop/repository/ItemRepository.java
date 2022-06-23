58462ceb2d4bf0d498dfda9cf055a79b109df141
이성현 <marvel97@naver.com>
2022-06-23 14:27:16 +0900

    delete all

--- src/main/java/com/shop/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shop.repository;
-
-import com.shop.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
-}
