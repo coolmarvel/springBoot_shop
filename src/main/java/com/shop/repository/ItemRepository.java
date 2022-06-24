cbabda4ecdd5597a092c1f2e4bb43162963a1af7
이성현 <marvel97@naver.com>
2022-06-24 11:25:52 +0900

    commit

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -11,15 +11,16 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long>,
         QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
 
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
 
-    List<Item> findByItemNmOrItemDetail(String itemNm,String itemDetail);
     List<Item> findByPriceLessThan(Integer price);
+
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
     @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
     List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
-    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc" , nativeQuery = true)
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
     List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
 }
